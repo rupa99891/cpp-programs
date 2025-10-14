e3693493d6875554b953658ed1ac12e7af01d93f
Rupadevi Deyyala <rupa07667@gmail.com>
2025-10-14 21:05:55 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void swap(int &x, int &y) {
+int temp = x;
+x = y;
+y = temp;
+}
+int main() {
+int a;
+int b;
+cout<<"enter a and b values:";
+cin>>a;
+cin>>b;
+cout << "Before swap: a = " << a << " b = " << b << endl;
+swap(a, b);
+cout << "After swap: a = " << a << " b = " << b << endl;
+return 0;
+}
+
